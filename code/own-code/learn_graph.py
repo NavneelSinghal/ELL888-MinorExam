b52380cc490437da476a1ba089721113d6ecd7d3
Navneel Singhal <navneel.singhal.iitd@gmail.com>
2022-02-24 01:48:09 +0530

    add solution to p2

--- code/own-code/learn_graph.py
+++ code/own-code/learn_graph.py
@@ -272,6 +272,144 @@ def l2_degree_reg(X, dist_type='sqeuclidean', alpha=1, s=None, step=0.5,
         return W
 
 
+# modification to the original solution where we can force distance between elements of separate classes to 0
+def l2_degree_reg_known_classes(X, classes, forced_distance=1e9, dist_type='sqeuclidean', alpha=1, s=None,
+                                step=0.5, w0=None, maxit=1000, rtol=1e-5, retall=False,
+                                verbosity='NONE'):
+    r"""
+    Learn graph by regularizing the l2-norm of the degrees.
+    We take into account the information about classes, i.e.,
+    set distance between nodes in different classes to infinity.
+
+    This is done by solving
+    :math:`\tilde{W} = \underset{W \in \mathcal{W}_m}{\text{arg}\min} \,
+    \|W \odot Z\|_{1,1} + \alpha \|W1}\|^2 + \alpha \| W \|_{F}^{2}`, subject
+    to :math:`\|W\|_{1,1} = s`, where :math:`Z` is a pairwise distance matrix,
+    and :math:`\mathcal{W}_m`is the set of valid symmetric weighted adjacency
+    matrices.
+
+    Parameters
+    ----------
+    X : array_like
+        An N-by-M data matrix of N variable observations in an M-dimensional
+        space. The learned graph will have N nodes.
+    classes : list of lists
+        classes[i] = list of nodes in class number i
+    forced_distance : float
+        Distance to be used for elements in different classes
+    dist_type : string
+        Type of pairwise distance between variables. See
+        :func:`spatial.distance.pdist` for the possible options.
+    alpha : float, optional
+        Regularization parameter acting on the l2-norm.
+    s : float, optional
+        The "sparsity level" of the weight matrix, as measured by its l1-norm.
+    step : float, optional
+        A number between 0 and 1 defining a stepsize value in the admissible
+        stepsize interval (see [Komodakis & Pesquet, 2015], Algorithm 6)
+    w0 : array_like, optional
+        Initialization of the edge weights. Must be an N(N-1)/2-dimensional
+        vector.
+    maxit : int, optional
+        Maximum number of iterations.
+    rtol : float, optional
+        Stopping criterion. Relative tolerance between successive updates.
+    retall : boolean
+        Return solution and problem details. See output of
+        :func:`pyunlocbox.solvers.solve`.
+    verbosity : {'NONE', 'LOW', 'HIGH', 'ALL'}, optional
+        Level of verbosity of the solver. See :func:`pyunlocbox.solvers.solve`.
+
+    Returns
+    -------
+    W : array_like
+        Learned weighted adjacency matrix
+    problem : dict, optional
+        Information about the solution of the optimization. Only returned if
+        retall == True.
+
+    Notes
+    -----
+    This is the problem proposed in [Dong et al., 2015], with the
+    modifications as specified in the solution to the
+    semi-supervised learning problem in the exam.
+
+
+    Examples
+    --------
+
+    """
+
+    # Parse X
+    N = X.shape[0]
+    E = int(N * (N - 1.) / 2.)
+    z = spatial.distance.pdist(X, dist_type)  # Pairwise distances
+
+    # Parse classes
+    k = len(classes)
+    for i in range(k):
+        for node_1 in classes[i]:
+            for j in range(i + 1, k):
+                for node_2 in classes[j]:
+                    ni, nj = min(node_1, node_2), max(node_1, node_2)
+                    z[N * ni + nj - ((ni + 2) * (ni + 1)) // 2] = forced_distance
+    
+    # Parse s
+    s = N if s is None else s
+
+    # Parse step
+    if (step <= 0) or (step > 1):
+        raise ValueError("step must be a number between 0 and 1.")
+
+    # Parse initial weights
+    w0 = np.zeros(z.shape) if w0 is None else w0
+    if (w0.shape != z.shape):
+        raise ValueError("w0 must be of dimension N(N-1)/2.")
+
+    # Get primal-dual linear map
+    one_vec = np.ones(E)
+
+    def K(w):
+        return np.array([2 * np.dot(one_vec, w)])
+
+    def Kt(n):
+        return 2 * n * one_vec
+
+    norm_K = 2 * np.sqrt(E)
+
+    # Get weight-to-degree map
+    S, St = utils.weight2degmap(N)
+
+    # Assemble functions in the objective
+    f1 = functions.func()
+    f1._eval = lambda w: 2 * np.dot(w, z)
+    f1._prox = lambda w, gamma: np.maximum(0, w - (2 * gamma * z))
+
+    f2 = functions.func()
+    f2._eval = lambda w: 0.
+    f2._prox = lambda d, gamma: s
+
+    f3 = functions.func()
+    f3._eval = lambda w: alpha * (2 * np.sum(w**2) + np.sum(S(w)**2))
+    f3._grad = lambda w: alpha * (4 * w + St(S(w)))
+    lipg = 2 * alpha * (N + 1)
+
+    # Rescale stepsize
+    stepsize = step / (1 + lipg + norm_K)
+
+    # Solve problem
+    solver = solvers.mlfbf(L=K, Lt=Kt, step=stepsize)
+    problem = solvers.solve([f1, f2, f3], x0=w0, solver=solver, maxit=maxit,
+                            rtol=rtol, verbosity=verbosity)
+
+    # Transform weight matrix from vector form to matrix form
+    W = spatial.distance.squareform(problem['sol'])
+
+    if retall:
+        return W, problem
+    else:
+        return W
+
 def glasso(X, alpha=1, w0=None, maxit=1000, rtol=1e-5, retall=False,
            verbosity='NONE'):
     r"""
